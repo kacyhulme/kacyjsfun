ee1420cb36ce0407d18db218c472cb9667f6144f
kacy hulme <kacyhulme@gmail.com>
2014-12-21 19:47:19 -0600

    changed function name to fit action better

--- kacyjscooking/cooking.js
+++ kacyjscooking/cooking.js
@@ -3,11 +3,11 @@ var spellItOut = function(){
   var length = letters.length;
   for (var i = 0; i < length; i++) {
     var x = 'p' + [i+1];
-    timeDelay(letters, length, i, x);
+    addLetter(letters, length, i, x);
   }
 }
 
-var timeDelay = function(arrayName, lengthOfArray, countNum, domEl){
+var addLetter = function(arrayName, lengthOfArray, countNum, domEl){
   setTimeout(function(){
     document.getElementById(domEl).innerHTML = arrayName[countNum];
   }, 400 * countNum);
