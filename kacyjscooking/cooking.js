f6ddfdbd9f6d9f631f4ad60a9d21d25d3231712b
kacy hulme <kacyhulme@gmail.com>
2014-12-21 18:57:45 -0600

    got 2 functions talking to eachother, but still not appearing on delay

--- kacyjscooking/cooking.js
+++ kacyjscooking/cooking.js
@@ -1,14 +1,16 @@
-function spellItOut(){
-  setTimeout(function() {
-    letters = ["A", "L", "M", "O", "N", "D", "S"];
-    length = letters.length;
-    console.log(length);
-    for (i = 0; i < length; i++) {
-      var x = 'p' + [i+1];
-      console.log(x);
-      document.getElementById(x).innerHTML = letters[i];
-    }
-  }, 800);
+var spellItOut = function(){
+  var letters = ["A", "L", "M", "O", "N", "D", "S"];
+  var length = letters.length;
+  for (var i = 0; i < length; i++) {
+    var x = 'p' + [i+1];
+    timeDelay(letters, length, i, x);
+  }
+}
+
+var timeDelay = function(arrayName, lengthOfArray, countNum, domEl){
+  setTimeout(function(){
+    document.getElementById(domEl).innerHTML = arrayName[countNum];
+  }, 1000);
 }
 
 spellItOut();
